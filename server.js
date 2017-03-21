73e7363df38f3e426dbf1b7c68ef67f34f98edb4
Jaison PP <pjaisonp@gmail.com>
2017-03-21 17:20:39 +0530

    setup start script PORT variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs')
 var app = express();
 
-const port = process.env.port||3000
+const port = process.env.PORT||3101
 
 app.get('/',(req,res)=>{
   res.send({

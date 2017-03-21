691b735bf89bcf81bb1c7917fbe75e73f3062367
Jaison PP <pjaisonp@gmail.com>
2017-03-21 13:42:08 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs')
+var app = express();
+
+app.get('/',(req,res)=>{
+  res.send({
+    name:'Jaison',
+    likes:[
+      'driving','dancing'
+    ]
+  })
+});
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs')
+
+app.use((req,res,nex)=>{
+  var now = new Date().toString();
+  console.log(`${now}:  ${req.method} ${req.url}`);
+
+  nex();
+});
+
+// app.use((req,res,next)=>{
+//   console.log(`Calling Maintain`);
+//   res.render('maintain.hbs',{
+//   pageTitle:'Maintain Mode'
+//   })
+// });
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(tasxt)=>{
+  return tasxt.toUpperCase();
+});
+
+
+app.get('/about',(req,res)=>{
+  //res.send('About page')
+  res.render('about.hbs',{
+    pageTitle:'About this page'
+    //currentYear:new Date().getFullYear()
+  })
+})
+app.get('/welcome',(req,res)=>{
+  res.render('welcome.hbs',{
+    pageTitle:'Welcome to Mustache',
+    UserName:'Jaison Pappachan',
+    welcomeMessage:'Poda Patti',
+    //currentYear:new Date().getFullYear(),
+    pageCount:200
+  })
+})
+app.get('/bad',(req,res)=>{
+  res.send({
+    error:'Error Handling'
+  })
+})
+app.listen(3100,()=>{
+  console.log('Server is up on 3100')
+});
+
+
+//
+// var express = require('express')
+// var app = express()
+//
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+//
+// app.listen(3100, function () {
+//   console.log('Example app listening on port 3000!')
+// })
